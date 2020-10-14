b6b29bd4739aabc2aee7faa646a82f74759fb5ab
Jasper Anders <jasper.anders@online.de>
2020-10-14 16:45:34 +0200

    removed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
